1ba2fdd920bc5cd080bbed44381e346cb4a75e20
신상욱 <sangwook@sagudori.local>
2024-05-23 17:58:12 +0900

    # ufo 추가

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,7 +22,7 @@ module.exports = {
         repo: 'git@github.com:sagudori/toy', // Git 리포지토리 URL
         path: '/game/www/event-v2', // 서버에서 애플리케이션이 배포될 경로
         'pre-deploy-local': '',
-        'post-deploy': 'pnpm install && pnpm run build && pm2 reload ecosystem.config.js --env production',
+        'post-deploy': 'cp ~/ecosystem.config.js ~/game/www/event-v2/current && pnpm install && pnpm run build && pm2 reload ecosystem.config.js --env production',
         'pre-setup': ''
       }
     }
