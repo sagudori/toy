17644c9db25b0206e87ece39f14dbf90fed51263
신상욱 <sangwook@sagudori.local>
2024-05-24 02:11:28 +0900

    edit ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,13 @@ module.exports = {
     apps: [
       {
         name: 'nuxt-app',
-        script: 'npm',
+        script: 'nuxt',
         args: 'start',
         instances: 4,
         exec_mode: 'cluster',
-        watch: false,
+        max_restarts: 3,
+        watch: true,
+        ignore_watch: ['node_modules', 'logs'],
         env: {
           NODE_ENV: 'development'
         },
@@ -17,20 +19,13 @@ module.exports = {
     ],
     deploy: {
       production: {
-        user: 'root', // 서버의 SSH 사용자명
+        user: 'root',
         host: [
           '172.19.0.3'
         ],
-        ref: 'origin/master', // Git 브랜치 이름
-        repo: 'git@github.com:sagudori/toy', // Git 리포지토리 URL
-        path: '/game/www/event-v2', // 서버에서 애플리케이션이 배포될 경로
-        // 'pre-deploy-local': 'ssh-keyscan -H 172.19.0.3 >> ~/.ssh/known_hosts',
-        // 'post-deploy': 'cp ~/ecosystem.config.js ~/game/www/event-v2/current && pnpm install && pnpm run build && pm2 reload ecosystem.config.js --env production',
-        // 'pre-setup': '',
-        // 'post-setup': 'echo "Setup is complete"',
-        // 'pre-deploy': 'echo "Deploying to production"',
-        // 'pre-restart': 'echo "Restarting application"',
-        // 'post-restart': 'echo "Application restarted"'
+        ref: 'origin/master',
+        repo: 'git@github.com:sagudori/toy',
+        path: '/game/www/event-v2',
         'pre-setup': 'mkdir -p /game/www/event-v2/source /game/www/event-v2/releases /game/www/event-v2/shared',
         'post-deploy': 'cp /game/www/event-v2/ecosystem.config.js /game/www/event-v2/current/ecosystem.config.js && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
         'post-restart': 'rm -rf /game/www/event-v2/source'
