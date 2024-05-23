bbd0ef6871a60c39e643099c724d8b51070de7d2
신상욱 <sangwook@sagudori.local>
2024-05-23 22:54:22 +0900

    ecosystem.config.js 배포시점 내용 변경

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -21,9 +21,16 @@ module.exports = {
         ref: 'origin/master', // Git 브랜치 이름
         repo: 'git@github.com:sagudori/toy', // Git 리포지토리 URL
         path: '/game/www/event-v2', // 서버에서 애플리케이션이 배포될 경로
-        'pre-deploy-local': '',
+        // 'pre-deploy-local': 'ssh-keyscan -H 172.19.0.3 >> ~/.ssh/known_hosts',
+        // 'post-deploy': 'cp ~/ecosystem.config.js ~/game/www/event-v2/current && pnpm install && pnpm run build && pm2 reload ecosystem.config.js --env production',
+        // 'pre-setup': '',
+        // 'post-setup': 'echo "Setup is complete"',
+        // 'pre-deploy': 'echo "Deploying to production"',
+        // 'pre-restart': 'echo "Restarting application"',
+        // 'post-restart': 'echo "Application restarted"'
+        'pre-deploy': 'mkdir -p /game/www/event-v2/source',
         'post-deploy': 'cp ~/ecosystem.config.js ~/game/www/event-v2/current && pnpm install && pnpm run build && pm2 reload ecosystem.config.js --env production',
-        'pre-setup': ''
+        'post-restart': 'rm -rf /game/www/event-v2/source'
       }
     }
   };
